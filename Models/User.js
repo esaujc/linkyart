d52b3ab883522aff32655701a107e71d0d3c055b
Bjornhona <asaeri3@hotmail.com>
2018-10-20 15:26:12 +0200

    add new files models, routes, views, middleware etc.

---
+++ Models/User.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+const Schema   = mongoose.Schema;
+const ObjectId = Schema.Types.ObjectId;
+
+const userSchema = new Schema ({
+    username: String,
+    password: String,
+    artist: Boolean,
+    space: Boolean
+  });
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
